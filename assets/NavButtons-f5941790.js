e2e81baca42b7fae5551427d742631deea4f36aa
Andrey-Golubenko <andrey.golubenko.it@gmail.com>
2024-04-04 03:03:03 +0200

    Updates

---
+++ assets/NavButtons-f5941790.js
@@ -0,0 +1,1 @@
+import{r as o,j as t,S as e}from"./vendor-b73bc09a.js";import{P as s}from"./get-video-id.esm-6ba5b9cc.js";const x=o.forwardRef((n,r)=>t.jsxs(e,{flexDirection:"row",justifyContent:"space-around",children:[t.jsxs(s,{buttonId:"previous",ref:r,children:["< "," Previous"]}),t.jsxs(s,{buttonId:"next",ref:r,children:["Next ",">"]})]}));export{x as N};
